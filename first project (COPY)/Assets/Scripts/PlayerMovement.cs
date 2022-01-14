b28cb9ebf29c9d3e2d7b0f24311c1f524e531716
erasevfx <dantemartin8256@gmail.com>
2022-01-13 21:27:53 -0600

    Added Cube Run Project

---
+++ first project (COPY)/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if ( Input.GetKey("d") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if ( Input.GetKey("a") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if ( Input.GetKey("left") )
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if ( Input.GetKey("right") )
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (rb.position.y < -10f)
+        {
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+}
